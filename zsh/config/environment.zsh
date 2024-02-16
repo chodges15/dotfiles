ff4293e323e8a543c20a14f9c862cb81f6d75dd4
Chris Hodges <chodges@dropbox.com>
2024-02-16 17:40:30 +0000

    new alias and tweaks

--- zsh/config/environment.zsh
+++ zsh/config/environment.zsh
@@ -1,2 +1,6 @@
 export EDITOR=/usr/bin/vim
 export TZ=UTC
+export XDG_CACHE_HOME="$HOME"/.cache
+export XDG_CONFIG_HOME="$HOME"/.config
+export XDG_DATA_HOME="$HOME"/.local/share
+export XDG_RUNTIME_DIR="$HOME"/.config/systemd/user
