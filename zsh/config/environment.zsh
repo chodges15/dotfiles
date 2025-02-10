30ee138b067606c2bc9010ade3b3e9923cf6824d
Chris Hodges <chris.hodges@gmail.com>
2025-02-10 00:04:41 +0000

    sops + direnv

--- zsh/config/environment.zsh
+++ zsh/config/environment.zsh
@@ -4,3 +4,6 @@ export XDG_CACHE_HOME="$HOME"/.cache
 export XDG_CONFIG_HOME="$HOME"/.config
 export XDG_DATA_HOME="$HOME"/.local/share
 export XDG_RUNTIME_DIR="$HOME"/.config/systemd/user
+export SOPS_PGP_FP="1388D4ECFDEC119999DE814F3BC63B1C49472D75"
+export GPG_TTY=$(tty)
+$(sops -d ~/sops.env.gpg)
