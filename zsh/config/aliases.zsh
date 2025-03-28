f9f6c632f3d67bb734a600deec8a3a3f9df7cdfd
Chris Hodges <chris.hodges@gmail.com>
2025-03-28 20:46:42 +0000

    alias pg
    alias pg
    
    alias pg
    
    alias pg

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -1,5 +1,6 @@
 alias a="cat ~/.gitconfig | sed -n '/\[alias\]/,/\[/p'"
 alias g="git"
+alias pg='GIT_SSH_COMMAND="ssh -F /dev/null -i ~/.ssh/pub_gh_id_ed25519 -o IdentitiesOnly=yes" git'
 alias k="kubectl"
 alias ctx='function _ctx() { kubectl config set-context --current --namespace="$1"; }; _ctx'
 alias recent='echo -e "\033[0;32mnewest\033[0m"; history | grep '\''^[ ]*[0-9]*[ ]*g gob'\'' | tail -n 10 | awk '\''{print "- " $NF}'\'' | tac'
