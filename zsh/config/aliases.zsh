0440f852828ca468026b0cc09f713edef868d740
Chris Hodges <chodges@dropbox.com>
2025-08-04 18:41:49 +0000

    env setup alias

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -4,3 +4,4 @@ alias pg='GIT_SSH_COMMAND="ssh -F /dev/null -i ~/.ssh/pub_gh_id_ed25519 -o Ident
 alias k="kubectl"
 alias ctx='function _ctx() { kubectl config set-context --current --namespace="$1"; }; _ctx'
 alias recent='echo -e "\033[0;32mnewest\033[0m"; history | grep '\''^[ ]*[0-9]*[ ]*g gob'\'' | tail -n 10 | awk '\''{print "- " $NF}'\'' | tac'
+alias env="set -a && source ~/.env && set +a"
