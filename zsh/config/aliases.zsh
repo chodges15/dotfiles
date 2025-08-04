228b1e236d813ac0b6d928d370d80d231aa0e749
chodges15 <chris.hodges@gmail.com>
2025-08-04 19:20:22 +0000

    update sops to alias

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -5,3 +5,4 @@ alias k="kubectl"
 alias ctx='function _ctx() { kubectl config set-context --current --namespace="$1"; }; _ctx'
 alias recent='echo -e "\033[0;32mnewest\033[0m"; history | grep '\''^[ ]*[0-9]*[ ]*g gob'\'' | tail -n 10 | awk '\''{print "- " $NF}'\'' | tac'
 alias env="set -a && source ~/.env && set +a"
+alias pgp='sops -d ~/sops.env.gpg'
