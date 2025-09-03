e3391df6b72e78628d709fd5a46c74b31f189428
chodges15 <chris.hodges@gmail.com>
2025-09-03 19:25:11 +0000

    sync mac

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -6,3 +6,5 @@ alias ctx='function _ctx() { kubectl config set-context --current --namespace="$
 alias recent='echo -e "\033[0;32mnewest\033[0m"; history | grep '\''^[ ]*[0-9]*[ ]*g gob'\'' | tail -n 10 | awk '\''{print "- " $NF}'\'' | tac'
 alias env="set -a && source ~/.env && set +a"
 alias pgp='sops -d ~/sops.env.gpg'
+alias c="claude"
+alias cr="claude --continue"
